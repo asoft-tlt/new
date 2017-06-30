1c1bd904bfc78788fd1d5e9e3e010bdd3759b091
asoft-tlt <a-soft@bk.ru>
2017-06-30 12:29:39 +0400

    Add files via upload

--- scripts/app-menu.js
+++ scripts/app-menu.js
@@ -165,7 +165,8 @@ window.App = {
     switch (page.type) {
       case 'video':
         var videoPath = 'video/pages/' + page.id + '/' + page.video;
-        content = '<video src="' + videoPath + '"><source src="' + videoPath + '" type="video/mp4;"></source></video>';
+         var videoPathMov = 'video/pages/' + page.id + '/' + page.video.replace("mp4", "mov");;
+        content = '<video src="' + videoPath + '"  autoplay="autoplay"><source src="' + videoPathMov + '" type="video/mov"><source src="' + videoPath + '" type="video/mp4;"></source></video>';
         break;
       default:
         content = this.Templates.get(page.template);
